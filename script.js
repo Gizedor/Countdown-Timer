9bffac0fd99253418da105a2b136ed213071e4eb
Gizedor <Gizedor90@gmail.com>
2021-07-13 08:20:20 +0300

    Complete

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
 const newYears = '1 jan 2022';
 
 function countdown() {
@@ -5,12 +10,21 @@ function countdown() {
     const currentDate = new Date();
 
     const totalseconds = (newYearsDate - currentDate) / 1000;
+
     const days = Math.floor(totalseconds / 3600 / 24);
     const hours = Math.floor(totalseconds / 3600) % 24;
-    const minutes = Math.floor(totalseconds / 60) % 60;
+    const mins = Math.floor(totalseconds / 60) % 60;
     const seconds = Math.floor(totalseconds) % 60;
 
-    console.log(days, hours, minutes, seconds);
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secondsEl.innerHTML = seconds;
+    // console.log(days, hours, minutes, seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time;
 }
 
 countdown();
